820afae0c328599316ce58e1285a1d4f821fff91
liamsinc <80267741+liamsinc@users.noreply.github.com>
2022-10-18 10:26:26 +0100

    Changed html files to php and included the header in all files to minimise repeated code

---
+++ about.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>About Me - Liam Sinclair</title>
+</head>
+<body>
+    <div class="site__container">
+        <?php include 'header.php' ?>
+        <div class="main">
+            <section class="about__section">
+                <h1 class="about__header">About Me</h1>
+                <p>Coming Soon...</p>
+            </section>
+        </div>
+    </div>
+</body>
+</html>
