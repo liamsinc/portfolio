9c15512b2f904ee9feab9f1e036d0bcd3ee7b329
liamsinc <80267741+liamsinc@users.noreply.github.com>
2022-11-02 10:53:05 +0000

    Major code refactoring, variables and function defined, code condensed where possible

--- js/app.js
+++ js/app.js
@@ -3,8 +3,84 @@ This file holds all the javascript/jquery functionality for index.html (homepage
 Functionally seperate code will be seperated into sections by "// COMMENTS --------".
 */
 
+// VARIABLES/CONSTANTS ---------------------------------------------------------------------------------
+
 // Get the page height:
 const pageHeight = $(document.body).prop('scrollHeight');
+
+// Color variables used when styling form messages:
+const successGreen = '#66FF00';
+const errorRed = '#EE4B2B';
+
+// Color variable used for active nav link:
+const aqua = '#00FFFF';
+
+// Are the form fields valid?
+let nameValid = true;
+let emailValid = true;
+let phoneValid = true;
+let subjectValid = true;
+let messageValid = true;
+
+// Is the form valid?
+let formValid = true;
+
+/*
+Regular expressions used to validate email and phone:
+1. Is the email formatted correctly?
+2. Does the phone number contain prohibited characters?
+3. Is the phone number in an accepted format?
+*/
+const emailRegex = /^[_\.0-9a-zA-Z-]+@([0-9a-zA-Z][0-9a-zA-Z-]+\.)+[a-zA-Z]{2,6}$/; 
+const phoneCharRegex = /^[\d \-()]*$/; 
+const phoneFormatRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+
+// Styles for input field success message:
+const fieldValidStyles = {
+    'color': successGreen,
+    'text-shadow': '0 0 10px', successGreen
+}
+
+// Styles for input field failure message:
+const fieldInvalidStyles = {
+    'color': errorRed,
+    'text-shadow': '0 0 10px', errorRed
+}
+
+// Styles for form success message:
+const formValidStyles = {
+    'color': successGreen,
+    'border-color': successGreen
+}
+
+// Styles for form failure message:
+const formInvalidStyles = {
+    'color': errorRed,
+    'border-color': errorRed
+}
+
+// Styles for active navigation link:
+const activeNavStyles = {
+    'color': aqua,
+    'text-decoration': 'underline'
+}
+
+// Configure settings for DOMPurify:
+const config = {
+    ALLOWED_TAGS: ['', ''],
+    KEEP_CONTENT: true
+};
+
+// Strings used in form error/success messages:
+const nameNullError = "Please enter your name!";
+const emailNullError = "Please enter your email!";
+const emailFormatError = "Please enter a valid email! (Invalid Format)";
+const phoneNullError = "Please enter your phone number!";
+const phoneCharError = "Please enter a valid phone number! (Prohibited Characters)";
+const phoneFormatError = "Please enter a valid phone number! (Invalid Format)";
+const subjectNullError = "Please enter your subject!";
+const messageNullError = "Please enter your message!";
+const fieldValidated = "Validation successful!";
 
 // FUNCTIONS ------------------------------------------------------------------------------------------
 
@@ -34,30 +110,6 @@ function checkHeaderScroll() {
 
 // FORM JAVASCRIPT/JQUERY START -----------------------------------------------------------------------
 
-// Local color variables used when styling form message:
-const successGreen = '#66FF00';
-const errorRed = '#EE4B2B';
-
-// Are the form fields valid?
-let nameValid = true;
-let emailValid = true;
-let phoneValid = true;
-let subjectValid = true;
-let messageValid = true;
-
-// Is the form valid?
-let formValid = true;
-
-/*
-Regular expressions used to validate email and phone:
-1. Is the email formatted correctly?
-2. Are there any prohibited characters in the phone number?
-3. Is the phone number in an accepted format?
-*/
-const emailRegex = /^[_\.0-9a-zA-Z-]+@([0-9a-zA-Z][0-9a-zA-Z-]+\.)+[a-zA-Z]{2,6}$/; 
-const phoneCharRegex = /^[\d \-()]*$/; 
-const phoneFormatRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
-
 // Hide the contact form message and validation error elements by default:
 $('.contact__message').hide();
 $('.contact__error').hide();
@@ -67,18 +119,15 @@ $('.contact__button').on('click', (event) => {
     // Prevent page reload:
     event.preventDefault();
 
+    // Ensure error message styles are reset:
+    $('.contact__error').css(fieldInvalidStyles);
+
     // Grab data from form fields, trimming leading and trailing whitespace:
     let name = $('#name').val().trim();
     let email = $('#email').val().trim();
     let phone = $('#phone').val().trim();
     let subject = $('#subject').val().trim();
     let message = $('#message').val().trim();
-
-    // Configure settings for DOMPurify
-    const config = {
-        ALLOWED_TAGS: ['', ''],
-        KEEP_CONTENT: true
-    };
 
     // Sanitize the user input (name will be displayed in HTML later, protects against XSS)
     name = DOMPurify.sanitize(name, config);
@@ -87,13 +136,13 @@ $('.contact__button').on('click', (event) => {
     subject = DOMPurify.sanitize(subject, config);
     message = DOMPurify.sanitize(message, config);
 
-    // Ensure error message styles are reset:
-    $('.contact__error').css({
-        color: errorRed,
-        textShadow: '0 0 10px', errorRed
-    });
-
     // BEGIN FORM VALIDATION:
+    /* 
+    Note: The last else if clauses of each validation conditonal will display a success message,
+    but only on form fields which have already failed validation once, and the error message is 
+    already present. It also sets the {field}Valid variables to true as by this point we know the 
+    validation has failed at least once.
+    */
 
     /* 
     NAME:
@@ -101,18 +150,14 @@ $('.contact__button').on('click', (event) => {
     */
     if (name === '') {
         $('.err-name')
-            .text("Please enter your name!")
+            .text(nameNullError)
             .slideDown();
         nameValid = false;
     } else if (nameValid === false){
         $('.err-name')
-            .css({
-                color: successGreen,
-                textShadow: '0 0 10px', successGreen
-            })
-            .text("Validation successful!")
-            .delay(2000)
-            .slideUp();
+            .css(fieldValidStyles)
+            .text(fieldValidated)
+            .delay(2000).slideUp();
         nameValid = true;
     }
 
@@ -123,23 +168,19 @@ $('.contact__button').on('click', (event) => {
     */
     if (email === '') {
         $('.err-email')
-            .text("Please enter your email!")
+            .text(emailNullError)
             .slideDown();
         emailValid = false;
     } else if (!email.match(emailRegex)) {
         $('.err-email')
-            .text("Please enter a valid email!")
+            .text(emailFormatError)
             .slideDown();
         emailValid = false;
     } else if (emailValid === false) {
         $('.err-email')
-            .css({
-                color: successGreen,
-                textShadow: '0 0 10px', successGreen
-            })
-            .text("Validation successful!")
-            .delay(2000)
-            .slideUp();
+            .css(fieldValidStyles)
+            .text(fieldValidated)
+            .delay(2000).slideUp();
         emailValid = true;
     }
 
@@ -151,28 +192,24 @@ $('.contact__button').on('click', (event) => {
     */
     if (phone === '') {
         $('.err-phone')
-            .text("Please enter your phone number!")
+            .text(phoneNullError)
             .slideDown();
         phoneValid = false;
     } else if (!phone.match(phoneCharRegex)) {
         $('.err-phone')
-            .text("Please enter a valid phone number! (Prohibited characters)")
+            .text(phoneCharError)
             .slideDown();
         phoneValid = false;
     } else if (!phone.match(phoneFormatRegex)) {
         $('.err-phone')
-            .text("Please enter a valid phone number! (Invalid format)")
+            .text(phoneFormatError)
             .slideDown();
         phoneValid = false;
     } else if (phoneValid === false) { 
         $('.err-phone')
-            .css({
-                color: successGreen,
-                textShadow: '0 0 10px', successGreen
-            })
-            .text("Validation successful!")
-            .delay(2000)
-            .slideUp();
+            .css(fieldValidStyles)
+            .text(fieldValidated)
+            .delay(2000).slideUp();
         phoneValid = true;
     }
 
@@ -182,18 +219,14 @@ $('.contact__button').on('click', (event) => {
     */
     if (subject === '') {
         $('.err-subject')
-            .text("Please enter your subject!")
+            .text(subjectNullError)
             .slideDown();
         subjectValid = false;
     } else if (subjectValid === false){
         $('.err-subject')
-            .css({
-                color: successGreen,
-                textShadow: '0 0 10px', successGreen
-            })
-            .text("Validation successful!")
-            .delay(2000)
-            .slideUp();
+            .css(fieldValidStyles)
+            .text(fieldValidated)
+            .delay(2000).slideUp();
         subjectValid = true;
     }
 
@@ -203,18 +236,14 @@ $('.contact__button').on('click', (event) => {
     */
     if (message === '') {
         $('.err-message')
-            .text("Please enter your message!")
+            .text(messageNullError)
             .slideDown();
         messageValid = false;
     } else if (messageValid === false) {
         $('.err-message')
-            .css({
-                color: successGreen,
-                textShadow: '0 0 10px', successGreen
-            })
-            .text("Validation successful!")
-            .delay(2000)
-            .slideUp()
+            .css(fieldValidStyles)
+            .text(fieldValidated)
+            .delay(2000).slideUp()
         messageValid = true;
     }
 
@@ -230,36 +259,24 @@ $('.contact__button').on('click', (event) => {
     /*
     When formValid is true, reset the contents of the input fields upon submit.
     Because I called event.preventDefault(), the page doesn't reload and thus the
-    form fields do not reset.
+    form fields do not reset automatically. Also sets the html content and styles 
+    of the form success/failure message dependant on form validation results.
     */
     if (formValid) {
         $('.contact__input').val('');
-    }
-
-    /* 
-    Sets the html content and styles dependant on form validation results.
-    Using object literals to apply multiple css styles and use local variables:
-    */
-    if (formValid) {
         $('.contact__message')
             .html(`<strong>Thanks ${name}! Your message has been sent successfully!</strong>`)
-            .css({
-                color: successGreen,
-                borderColor: successGreen,
-            });
+            .css(formValidStyles);
     } else {
         $('.contact__message')
             .html("<strong>Oops! Please check your form fields!</strong>")
-            .css({
-                color: errorRed,
-                borderColor: errorRed,
-            });
+            .css(formInvalidStyles);
     }
 
     /*
     After setting the message and styles, animate the message transition,
     but only if the element is not already visible.
-    Manipulates the submit button.
+    Also manipulates the submit button.
     Prevents the animation from repeatedly running in certain circumstances:
     */ 
     if($('.contact__message').is(":hidden")) {
@@ -271,10 +288,32 @@ $('.contact__button').on('click', (event) => {
 
 // FORM JAVASCRIPT/JQUERY END -----------------------------------------------------------------------
 
-// SIDE MENU JAVASCRIPT/JQUERY START ----------------------------------------------------------------
+// GENERIC JAVASCRIPT/JQUERY START ------------------------------------------------------------------
 
-// Unchecks the menu button, clears form fields and checks header button on page reload:
+/*
+When the page loads/reloads, get the URL and apply active styles to relevant nav link:
+The conditional will keep the homepage as the active link when "my portfolio" or "contact me" 
+links are clicked, as the elements they refer to are on the same page. The conditional also 
+accounts for a bugs that occurs when the homepage is viewed via github pages, cPanel 
+domain or VSC live server (see fourth/fifth/sixth statement in the first conditional).
+After the conditional, it unchecks the menu button, clears form fields and checks if the header 
+scroll button should be visible:
+*/
 $(document).ready(() => {
+    const currentURL = window.location.href;
+    if (currentURL.endsWith('index.html') || currentURL.endsWith('#portfolio') ||
+        currentURL.endsWith('#form-anchor') || currentURL.endsWith('portfolio/') || 
+        currentURL.endsWith('scs.co.uk/') || currentURL.endsWith(':5500/')) { 
+        $('#home-link').css(activeNavStyles);
+    } else if (currentURL.endsWith('about.html')) {
+        $('#about-link').css(activeNavStyles);
+    } else if (currentURL.endsWith('examples.html')) {
+        $('#examples-link').css(activeNavStyles);
+    } else if (currentURL.endsWith('scs.html')){
+        $('#scs-link').css(activeNavStyles);
+    } else {
+        console.log(`Error: Cannot determine active link for ${currentURL}`);
+    }
     $('#check').prop('checked', false);
     $('.contact__input').val('');
     checkHeaderScroll();
@@ -289,12 +328,20 @@ $('#check').on('click', () => {
     } 
 });
 
+// Attaches pre-built function to scroll to the top of the page
+$('.header__button').on('click', jumpToTop);
+$('.mobile__scroll').on('click', jumpToTop);
+
+// Closes side menu on small devices when page local links are clicked:
+$('#portfolio-link').on('click', closeSideMenu);
+$('#contact-link').on('click', closeSideMenu);
+
 /*
 When a browser window resize event is triggered,
 check if the viewport width is greater than 768px.
 If true, uncheck the checkbox and show the header.
 Else if the viewport width is less than 768px AND the menu button is unchecked, 
-hide the header.
+hide the header. After conditional, run checkHeaderScroll().
 */
 $(window).on('resize', () => {
     if (window.innerWidth >= 768) {
@@ -306,62 +353,4 @@ $(window).on('resize', () => {
     checkHeaderScroll();
 });
 
-// Closes side menu on small devices when page local links are clicked:
-
-$('#portfolio-link').click(closeSideMenu);
-$('#contact-link').click(closeSideMenu);
-
-// SIDE MENU JAVASCRIPT/JQUERY END ----------------------------------------------------------------
-
-// GENERIC JAVASCRIPT/JQUERY START ----------------------------------------------------------------
-
-// Color variable used for active nav link:
-const aqua = '#00FFFF';
-
-/*
-When the page loads, get the URL and apply active styles to relevant nav link:
-The conditional will keep the homepage as the active link when "my portfolio" or "contact me" links are clicked, as the elements they refer to are on the same page. 
-The conditional also accounts for a bugs that occurs when the homepage is viewed via github pages or the cPanel domain (see fourth/fifth statement in the first conditional).
-*/
-$(document).ready(() => {
-    const currentURL = window.location.href;
-    if (currentURL.endsWith('index.html') ||
-        currentURL.endsWith('#portfolio') ||
-        currentURL.endsWith('#form-anchor') ||
-        currentURL.endsWith('portfolio/') || // Fixes bug when page is loaded via github pages
-        currentURL.endsWith('scs.co.uk/') || // Fixes bug when page is loaded via cPanel domain
-        currentURL.endsWith(':5500/')) { // Fixes rare bug when page is loaded via VSC live server
-
-        $('#home-link').css({
-            color: aqua,
-            textDecoration: 'underline'
-        });
-    } else if (currentURL.endsWith('about.html')) {
-        $('#about-link').css({
-            color: aqua,
-            textDecoration: 'underline',
-        });
-    } else if (currentURL.endsWith('examples.html')) {
-        $('#examples-link').css({
-            color: aqua,
-            textDecoration: 'underline'
-        });
-    } else if (currentURL.endsWith('scs.html')){
-        $('#scs-link').css({
-            color: aqua,
-            textDecoration: 'underline'
-        });
-    } else {
-        console.log(`Error: Cannot determine active link for ${currentURL}`);
-    }
-});
-
-
-
-
-// Attaches pre-built function to scroll to the top of the page
-$('.header__button').on('click', jumpToTop);
-$('.mobile__scroll').on('click', jumpToTop);
-
 // GENERIC JAVASCRIPT/JQUERY END ------------------------------------------------------------------
-
